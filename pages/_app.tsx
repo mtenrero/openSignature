5390561fbb3bdfcbd956dd53eb1fccc16a3af288
Marcos Tenrero <tenrero@aol.com>
2021-11-21 23:13:14 +0100

    Basic Design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { Grommet, Main, Header, Button, Menu } from 'grommet'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet plain>
+      <Main
+        border={{ color: 'brand', size: 'large' }}
+      >
+        <Header background="brand">
+          <Button hoverIndicator label="OpenSignature"/>
+          <Menu label="account" items={[{ label: 'logout' }]} />
+        </Header>
+        <Component {...pageProps} />
+      </Main>
+    </Grommet>
+  )
+  
+  
 }
 
 export default MyApp
