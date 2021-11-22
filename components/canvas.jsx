046bd6879d85874749e52a461b7a30621a430012
Marcos Tenrero <tenrero@aol.com>
2021-11-22 01:56:46 +0100

    contract UI signature + responsive

---
+++ components/canvas.jsx
@@ -0,0 +1,14 @@
+import React, { useRef } from 'react'
+
+const Canvas = props => {
+  const canvasRef = useRef(null)
+  
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+  }, [])
+  
+  return <canvas ref={canvasRef} {...props}/>
+}
+
+export default Canvas
--- pages/sign/index.tsx
+++ pages/sign/index.tsx
@@ -1,22 +1,53 @@
 import { Card, CardHeader, CardBody, CardFooter, Button, CheckBox, ResponsiveContext, Heading, Paragraph, Box } from "grommet";
-import { margin } from "polished";
 import React from "react";
+import SignaturePad from "react-signature-pad-wrapper";
+
 
 export default function SignDocument(props: any) {
     const size = React.useContext(ResponsiveContext);
+    const Canvas = props => <canvas {...props}/>
+
     return(
         <Box margin={{bottom: "10px"}}>
             <Heading textAlign="center">Contract Name</Heading>
-            <Card alignSelf="center" height={size} width={{min: "60%", max: "90%"}} background="light-1">
+            <Card alignSelf="center" height={{min: "80%"}} width={{min: "60%", max: "90%"}} background="light-1">
                 <CardBody pad={size} style={{textAlign: "justify"}}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                     labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua.
+
+                    <div style={{margin:"15px"}}>
+
+                    <Heading level="3" margin="none">Signature</Heading>
+                    <CheckBox
+                        label="I accept the terms of this contract"
+                        style={{marginTop: "10px"}}
+                    />
+                    <Box 
+                        border
+                        style={{marginTop: "10px"}}
+                    >
+                        <SignaturePad  redrawOnResize canvasProps={{
+                            backgroundColor: 'rgb(255, 255, 255)'
+                        }}/>
+                    </Box>
+                    </div>
+                    
                 
                 </CardBody>
                 <CardFooter background="light-2">
-                    <CheckBox
-                        label="I accept the terms of this contract"
-                    />
+                    
                     <Button primary margin="5px" hoverIndicator label="Sign" />
                 </CardFooter>
             </Card>
