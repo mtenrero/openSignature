fd189df1a30a9e77dce9eb5730baf3cc745d3353
Marcos Tenrero <tenrero@aol.com>
2021-12-12 13:39:24 +0100

    propagate tenantErr

--- pages/api/esign/_middleware.ts
+++ pages/api/esign/_middleware.ts
@@ -1,19 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 import findTenantByToken from '../../../libs/findToken'
+import { goHandle } from '../../../libs/goHandle'
 
 export async function middleware(req: NextRequest) {
   const auth = req.headers.get('Authorization')
 
   if (auth) {
     const authToken = auth.replace("Bearer ", "")
-    const tenant = await findTenantByToken(authToken).catch(()=> {
-       return null
-     })
+    const [tenant, tenantErr] = await goHandle(findTenantByToken(authToken))
 
     if (tenant !== null) {
       return NextResponse.next()
     } else {
-      return new Response('Authorization failed', {
+      return new Response('Authorization failed' + tenantErr, {
         status: 403,
       })
     }
